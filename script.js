1a51e47d967b4e787fb8c7315fe9c53f1a60ab8c
MarionHolloway <32263617+MarionHolloway@users.noreply.github.com>
2023-04-08 18:25:01 +0100

    Creat 16x16 grid of divs

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const container = document.querySelector('#container');
 
-const content = document.createElement('div');
-content.style.backgroundColor = "blue";
-content.classList.add('pixel');
+const pixel = document.createElement('div');
+pixel.style.backgroundColor = "blue";
+pixel.classList.add('pixel');
 
+const row = document.createElement('div');
+row.style.backgroundColor = "green";
+row.classList.add('row');
 for (let size = 0; size < 16; size++) {
-    container.appendChild(content.cloneNode(true));
+    row.appendChild(pixel.cloneNode(true));
+    
+}
+
+for (let rows = 0; rows < 16; rows++) {
+    container.appendChild(row.cloneNode(true));
 }

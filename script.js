6fb621216ce3af759e5a241826f4fcd1b1da155e
MarionHolloway <32263617+MarionHolloway@users.noreply.github.com>
2023-04-09 09:43:42 +0100

    Refactor code into createGrid function

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+function createGrid(container) {
+    const pixel = document.createElement('div');
+    pixel.style.backgroundColor = "blue";
+    pixel.classList.add('pixel');
+
+    const row = document.createElement('div');
+    row.style.backgroundColor = "green";
+    row.classList.add('row');
+
+    for (let size = 0; size < 16; size++) {
+        row.appendChild(pixel.cloneNode(true));
+        
+    }
+
+    for (let rows = 0; rows < 16; rows++) {
+        container.appendChild(row.cloneNode(true));
+    }
+}
+
 const container = document.querySelector('#container');
-
-const pixel = document.createElement('div');
-pixel.style.backgroundColor = "blue";
-pixel.classList.add('pixel');
-
-const row = document.createElement('div');
-row.style.backgroundColor = "green";
-row.classList.add('row');
-for (let size = 0; size < 16; size++) {
-    row.appendChild(pixel.cloneNode(true));
-    
-}
-
-for (let rows = 0; rows < 16; rows++) {
-    container.appendChild(row.cloneNode(true));
-}
+createGrid(container);
